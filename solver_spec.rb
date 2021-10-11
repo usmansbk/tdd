3b946ef8c02580dca5f713226389679a29fce399
Usman Suleiman <usmansbk@gmail.com>
2021-10-11 23:11:25 +0100

    Write fizzbuzz tests

--- solver_spec.rb
+++ solver_spec.rb
@@ -24,4 +24,36 @@ describe Solver do
       end
     end
   end
+
+  describe '#reverse' do
+    it 'should return the reversed word' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+    end
+  end
+
+  describe '#fizzbuzz' do
+    context 'when N is divisible by 3' do
+      it 'should return fizz' do
+        expect(@solver.fizzbuzz(3)).to eq 'fizz'
+      end
+    end
+
+    context 'when N is divisible by 5' do
+      it 'should return buzz' do
+        expect(@solver.fizzbuzz(5)).to eq 'buzz'
+      end
+    end
+
+    context 'when N is divisible by 3 and 5' do
+      it 'should return fizzbuzz' do
+        expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+      end
+    end
+
+    context 'any other case' do
+      it 'should return N as a string' do
+        expect(7).to eq '7'
+      end
+    end
+  end
 end
